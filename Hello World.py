d488d32101d0c1b91bcb3e7e578ac1d940fb5d9a
Mpinganzima <67333416+Mpinganzima@users.noreply.github.com>
2021-07-22 18:34:29 +0200

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class C:
+    def __init__(self, data):
+        self.data = data
+
+an_instance = C("Hello world , This is Rehema")
+print(an_instance.data)
+
+
+# In[ ]:
+
+
+
+
